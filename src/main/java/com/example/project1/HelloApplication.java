06273c57f82374acda7928d93ef579400478db0f
Jdavid77 <jdavid72009@hotmail.com>
2021-10-28 16:04:31 +0100

    MainView Edited!

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.project1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,10 +13,17 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            ScrollPane scrollPane = fxmlLoader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+            Scene mainscene = new Scene(scrollPane);
+            stage.setScene(mainscene);
+            stage.setTitle("Sample JavaFX Application");
+            stage.show();
+            /*Scene scene = new Scene(fxmlLoader.load(), 320, 240);
             stage.setTitle("Sample JavaFX Application!");
             stage.setScene(scene);
-            stage.show();
+            stage.show();*/
         }
         catch (IOException e){
             e.printStackTrace();
